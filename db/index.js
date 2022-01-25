7c2c43e87b0ed31d3e455e60ecfbe8be9704405c
Daisy Centeno <daisy.centeno13@gmail.com>
2022-01-24 20:35:04 -0800

    starting code for pt 2

--- db/index.js
+++ db/index.js
@@ -1,36 +1,31 @@
 // inside db/index.js
-const { Client } = require('pg'); // imports the pg module
+const { Client } = require('pg') // imports the pg module
 
-// supply the db name and location of the database
 const client = new Client('postgres://localhost:5432/juicebox-dev');
 
 async function createUser({ username, password }) {
   try {
-    const result = await client.query(`
+    const { rows } = await client.query(`
       INSERT INTO users(username, password) 
       VALUES($1, $2) 
       ON CONFLICT (username) DO NOTHING 
       RETURNING *;
     `, [username, password]);
 
-    return result;
+    return rows;
   } catch (error) {
     throw error;
   }
 }
 
 async function getAllUsers() {
-  const { rows } = await client.query(
-    `SELECT id, username 
-    FROM users;
-  `);
+  const { rows } = await client.query(`SELECT id, username FROM users;`);
 
   return rows;
 }
 
-// and export them
-module.exports = {
+module.exports = {  
   client,
+  createUser,
   getAllUsers,
-  createUser
 }
--- db/seed.js
+++ db/seed.js
@@ -2,7 +2,7 @@
 const {
   client,
   getAllUsers,
-  createUser
+  createUser,
 } = require('./index');
 
 async function dropTables() {
@@ -39,25 +39,20 @@ async function createTables() {
   }
 }
 
-// new function, should attempt to create a few users
 async function createInitialUsers() {
   try {
     console.log("Starting to create users...");
 
-    const albert = await createUser({ username: 'albert', password: 'bertie99' });
-    const sandra = await createUser({ username: 'sandra', password: '2sandy4me'});
-    const glamgal = await createUser({ username: 'glamgal', password: 'soglam'});
-    
-    console.log(albert);
+    await createUser({ username: 'albert', password: 'bertie99' });
+    await createUser({ username: 'sandra', password: '2sandy4me' });
+    await createUser({ username: 'glamgal', password: 'soglam' });
 
     console.log("Finished creating users!");
-  } catch(error) {
+  } catch (error) {
     console.error("Error creating users!");
     throw error;
   }
 }
-
-
 
 async function rebuildDB() {
   try {

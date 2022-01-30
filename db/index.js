05333ca52c3b897001e53bf9cc36e4c951293acb
Daisy Centeno <daisy.centeno13@gmail.com>
2022-01-30 15:37:18 -0800

    finished UPDATING A POST section of PART 3

--- db/index.js
+++ db/index.js
@@ -111,52 +111,52 @@ async function getUserById(userId) {
 }
 
 async function updatePost(postId, fields = {}) {
-  //read off the tags & remove that feild 
-  const {tags} = fields; // might be undefined 
-  delete fields.tags; 
+  // read off the tags & remove that field 
+  const { tags } = fields; // might be undefined
+  delete fields.tags;
 
   // build the set string
   const setString = Object.keys(fields).map(
     (key, index) => `"${ key }"=$${ index + 1 }`
   ).join(', ');
 
-    try{
-      //update any fields that need to be updated   
-      if(setString.length > 0) {
-        await client.query(`
-          UPDATE posts
-          SET ${ setString}
-          WHERE  id=${postId}
-          RETURNING *;
-        `, Object.values(fields));
-      } 
-
-      // return early if there's no tags to update
-      if (tags === undefined) {
-        return await getPostById(postId);
-      }
-
-      //make any new tags that need to be made
-      const tagList = await createTags(tags);
-      const tagListIdString = tagList.map(
-        tag => `${tag.id}`
-      ). join(', ');
-
-      //delete any post_tags from the database which aren't in the tagList
+  try {
+    // update any fields that need to be updated
+    if (setString.length > 0) {
       await client.query(`
-        DELETE FROM post_tags
-        WHERE "tagId"
-        NOT IN (${tagListIdString})
-        AND "postId"=$1;
-      `, [postId]);
-
-      // and create post_tags as necessary
-      await addTagsToPost(postId, tagList);
-
-      return await getPostById(postId);
-    }catch (error) {
-      throw error;
+        UPDATE posts
+        SET ${ setString }
+        WHERE id=${ postId }
+        RETURNING *;
+      `, Object.values(fields));
     }
+
+    // return early if there's no tags to update
+    if (tags === undefined) {
+      return await getPostById(postId);
+    }
+
+    // make any new tags that need to be made
+    const tagList = await createTags(tags);
+    const tagListIdString = tagList.map(
+      tag => `${ tag.id }`
+    ).join(', ');
+
+    // delete any post_tags from the database which aren't in that tagList
+    await client.query(`
+      DELETE FROM post_tags
+      WHERE "tagId"
+      NOT IN (${ tagListIdString })
+      AND "postId"=$1;
+    `, [postId]);
+
+    // and create post_tags as necessary
+    await addTagsToPost(postId, tagList);
+
+    return await getPostById(postId);
+  } catch (error) {
+    throw error;
+  }
 }
 
 async function getAllPosts() {
--- db/seed.js
+++ db/seed.js
@@ -200,6 +200,12 @@ async function testDB() {
     const albert = await getUserById(1);
     console.log("Result:", albert);
 
+    console.log("Calling updatePost on posts[1], only updating tags");
+    const updatePostTagsResult = await updatePost(posts[1].id, {
+      tags: ["#youcandoanything", "#redfish", "#bluefish"]
+    });
+    console.log("Result:", updatePostTagsResult);
+
     console.log("Finished database tests!");
   } catch (error) {
     console.log("Error during testDB");

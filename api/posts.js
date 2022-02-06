1cbabfe92998130b7f1e56ff0a37ced75e2dd8fc
Daisy Centeno <daisy.centeno13@gmail.com>
2022-02-06 13:51:45 -0800

    removed duplicate require user import and confirmed post route functionality via curl statement

--- api/posts.js
+++ api/posts.js
@@ -2,6 +2,12 @@
 const express = require('express');
 const postsRouter = express.Router();
 
+const { requireUser } = require('./utils');
+
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  res.send({ message: 'under construction' });
+});
+
 postsRouter.use((req, res, next) => {
   console.log("A request is being made to /posts");
 
@@ -11,13 +17,36 @@ postsRouter.use((req, res, next) => {
 // NEW
 const { getAllPosts } = require('../db');
 
-// UPDATE
-postsRouter.get('/', async (req, res) => {
-  const posts = await getAllPosts();
+postsRouter.post('/', requireUser, async (req, res, next) => {
+  const { title, content, tags = "" } = req.body;
 
-  res.send({
-    posts
-  });
+  const tagArr = tags.trim().split(/\s+/)
+  const postData = {};
+
+  // only send the tags if there are some to send
+  if (tagArr.length) {
+    postData.tags = tagArr;
+  }
+
+  try {
+    // add authorId, title, content to postData object
+
+    // const post = await createPost(postData);
+    // this will create the post and the tags for us
+    const posts = await createPost(postData);
+    // if the post comes back, res.send({ post });
+    // otherwise, next an appropriate error object 
+    if (posts) {
+      res.send({
+        posts
+      });
+    }else {
+      next({});
+    }
+    
+  } catch ({ name, message }) {
+    next({ name, message });
+  }
 });
 
 module.exports = postsRouter;

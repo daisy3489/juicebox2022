8796768db15109717153d3d717c291283d334e55
Daisy Centeno <daisy.centeno13@gmail.com>
2022-02-06 20:05:41 -0800

    changed client again

--- db/index.js
+++ db/index.js
@@ -1,7 +1,10 @@
 // inside db/index.js
 const { Client } = require('pg') // imports the pg module
 
-const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+});
 
 /**
  * USER Methods
